6d5d7ee5e48cf39a5635287d9887873c8cc36412
Reico Lee <leereico@gmail.com>
2016-11-14 12:23:51 -0500

    hooks

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const chalk = require('chalk');
+const app = express();
+const wikiRouter = require('./routes/wiki.js');
+const nunjucks = require('nunjucks');
+const models = require('./models');
+const port = 3000;
+
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment
+// instance, which we'll want to use to add Markdown support later.
+const env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+models.Users.sync({})
+.then(function () {
+    return models.Pages.sync({force:true})
+})
+.then(function () {
+    app.listen(port, function () {
+        console.log(`Server is listening on port ${port}!`);
+    });
+})
+.catch(console.error);
+
+app.get('/', function(req,res,next){
+  res.redirect("/wiki");
+})
+app.use("/wiki", wikiRouter);
+
+
--- models/index.js
+++ models/index.js
@@ -3,14 +3,13 @@ let db = new Sequelize('postgres://localhost:5432/wikistack',{
   logging: false
 });
 
-const Pages = sequelize.define('page' {
+const Pages = db.define('page', {
   title: {
     type: Sequelize.STRING,
     allowNull: false
   },
   urlTitle:{
     type: Sequelize.STRING,
-    isUrl: true,
     allowNull: false
   },
   content: {
@@ -18,12 +17,34 @@ const Pages = sequelize.define('page' {
     allowNull: false
   },
   status: {
-    type: Sequelize.STRING,
-    allowNull: true
+    type: Sequelize.ENUM('open', 'closed'),
+
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  }
+}, {
+  getterMethods: {
+    route: function(){
+      return "/wiki/" + this.getDataValue("urlTitle");
+    },
+  hooks: {
+      beforeValidate: function generateUrlTitle (title) {
+        if (title) {
+    // Removes all non-alphanumeric characters from title
+    // And make whitespace underscore
+          return title.replace(/\s+/g, '_').replace(/\W/g, '');
+        }else {
+    // Generates random 5 letter string
+          return Math.random().toString(36).substring(2, 7);
+        }
+      }
+    }
   }
 });
 
-const Users = sequelize.define('user' {
+const Users = db.define('user', {
    name: {
     type: Sequelize.STRING,
     is: ["^[a-z]+$",'i'],
